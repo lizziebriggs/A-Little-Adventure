bf58d578909b0541055d5d8ede5111262293bea1
Lizzie <lizzie.briggs10@gmail.com>
2019-10-19 16:24:36 +0100

    Character controls and camera control
    Created character movement controls and camera controls to follow the character

---
+++ Little-Big-World/Assets/Scripts/Actors/MainCharacters/CharacterController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // == Character Information Variables ==
+    [SerializeField] private Color colour;
+
+    // == Movement Variables ==
+    private Rigidbody rb;
+
+    [SerializeField] private float walkSpeed = 5;
+    [SerializeField] private float runSpeed = 10;
+    [SerializeField] private float acceleration = 2;
+    private float currentSpeed = 1;
+    private Vector3 directionToMove = Vector3.zero;
+    private Vector3 directionVector = Vector3.zero;
+
+    // == Character Management Variables ==
+    public bool isCurrentCharacter;
+    private bool canMove;
+
+    // == Camera Variables ==
+    [SerializeField] private Camera mainCamera;
+    private CameraController cameraController;
+
+
+    void Start()
+    {
+        if(GetComponent<Rigidbody>() != null)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        cameraController = mainCamera.GetComponent<CameraController>();
+
+        GetComponent<Renderer>().material.color = colour;
+
+        currentSpeed = walkSpeed;
+    }
+
+
+    void Update()
+    {
+        if (isCurrentCharacter && !cameraController.moveCamera && !cameraController.rotateCamera) canMove = true;
+        else canMove = false;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if(canMove) MoveCharacter();
+    }
+
+
+    private void MoveCharacter()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+            SpeedUp();
+        else
+            SlowDown();
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * currentSpeed, 0);
+        directionToMove.z = Input.GetAxis("Vertical");
+        directionVector = (rb.transform.right * directionToMove.x) + (rb.transform.forward * directionToMove.z);
+
+        rb.MovePosition(rb.transform.position + Time.deltaTime * currentSpeed * directionVector);
+
+    }
+
+
+    private void SlowDown()
+    {
+        if (currentSpeed >= walkSpeed)
+            currentSpeed -= acceleration * Time.deltaTime;
+    }
+
+
+    private void SpeedUp()
+    {
+        if (currentSpeed <= runSpeed)
+            currentSpeed += acceleration * Time.deltaTime;
+    }
+}
---
+++ Little-Big-World/Assets/Scripts/Actors/MainCharacters/CharacterManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    [SerializeField] private List<CharacterController> playableCharacters;
+    [SerializeField] private CameraController mainCamera;
+
+    private CharacterController currentCharacter;
+    private int currentCharacterIndex;
+
+
+    void Start()
+    {
+        currentCharacterIndex = 0;
+    }
+
+
+    void Update()
+    {
+        currentCharacter = playableCharacters[currentCharacterIndex];
+        currentCharacter.isCurrentCharacter = true;
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            ChangeCurrentCharacter();
+        }
+    }
+
+    
+    private void ChangeCurrentCharacter()
+    {
+        currentCharacter.isCurrentCharacter = false;
+        currentCharacterIndex++;
+
+        if (currentCharacterIndex > playableCharacters.Count - 1)
+            currentCharacterIndex = 0;
+
+        mainCamera.ChangeCharacterToFollow();
+    }
+}
