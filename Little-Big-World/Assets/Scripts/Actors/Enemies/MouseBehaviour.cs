efcb45692db1d3678ea7c6f06e82e449ec991cd9
Lizzie <lizzie.briggs10@gmail.com>
2020-01-08 09:56:34 +0000

    Created mouse white-box model and behaviour script

---
+++ Little-Big-World/Assets/Scripts/Actors/Enemies/MouseBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBehaviour : EnemyBase
+{
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        GetComponent<Renderer>().material.color = colour;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
