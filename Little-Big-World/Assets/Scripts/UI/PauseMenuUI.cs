373c02ca2bdfaaed9f5c9db00c0eb0aa11e8a848
Lizzie <lizzie.briggs10@gmail.com>
2020-01-08 09:48:29 +0000

    Pause menu hides/shows and added resume btn functionality

---
+++ Little-Big-World/Assets/Scripts/UI/PauseMenuUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu = null;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1) Show();
+
+            else if (Time.timeScale == 0) Hide();
+        }
+    }
+
+    public void Show()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
